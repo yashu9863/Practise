0a38411531632b2d89ab48c0618ad8eefb0dd54e
yashwanth <yashwanth@SUGGALA>
2017-07-06 16:51:18 -0500

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new CustomerModel() { Id = 1, Email = "test@test.com", Name = "Sai" };
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/CarModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class CarModel
+    {   
+        [Key]
+        public int CarId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Model { get; set; }
+
+        public string Company { get; set; }
+
+        public int CarNum { get; set; }
+    }
+}
